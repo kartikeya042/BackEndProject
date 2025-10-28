6b35b4f3aaedcf07910a42c873bdf4f895fb70a5
kartikeya042 <kartikeyasingh042@gmail.com>
2025-10-29 02:14:04 +0530

    fixed all register controller bugs.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,6 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import {APIError} from "../utils/apiError.js";
 import {User} from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
-import { upload } from "../middlewares/multer.middleware.js";
 import { APIResponse } from "../utils/apiResponse.js";
 
 const registerUser = asyncHandler( async (req, res) =>{
@@ -16,7 +15,7 @@ const registerUser = asyncHandler( async (req, res) =>{
     // check for user creation
     // return response
 
-    const {username, fullname, email, password} = req.body;
+    const {username, fullName, email, password} = req.body;
     console.log("Email:", email);
 
     // Basic method:
@@ -26,14 +25,14 @@ const registerUser = asyncHandler( async (req, res) =>{
 
     // Checking if all the fields are filled or not.
     if(
-        [fullname, email, password, username].some((field) => 
+        [fullName, email, password, username].some((field) => 
         field?.trim() === "")
     ){
         throw new APIError(400, "All fields are required.")
     }
 
     // Checking if a user already exists or not.
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{username}, {email}]
     })
     if(existedUser){
@@ -41,8 +40,11 @@ const registerUser = asyncHandler( async (req, res) =>{
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
-
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
     if (!avatarLocalPath) {
         throw new APIError(400, "Avatar file is required.");
     }
@@ -53,9 +55,9 @@ const registerUser = asyncHandler( async (req, res) =>{
     if(!avatar){
         throw new APIError(400, "Avatar file is required.");
     }
-
+    
     const user = await User.create({
-        fullname,
+        fullName,
         avatar: avatar.url,
         coverImage: coverImage?.url || "",
         email,
